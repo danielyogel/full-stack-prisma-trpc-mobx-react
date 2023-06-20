53a624c3ffb00a9cb8ea0b37351033129b1ea41d
Daniel Yogel <danielyogel@gmail.com>
2023-06-20 14:15:14 +0300

    deps

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. migrate from Tailwind to UnoCSS
+2. add fly.io CICD
+3. update libs 
---
+++ server/prisma/migrations/20230620105019_initial_migration/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Person_pkey" PRIMARY KEY ("id")
+);
